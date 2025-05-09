f629c9d745070f74e410d6767e79ae69ab7b33f0
Nicholas Cecere <nick@bitop.dev>
2025-05-09 15:13:11 -0400

    Add support for team member permissions in team resource (v3.1.0)

--- litellm/types.go
+++ litellm/types.go
@@ -31,16 +31,17 @@ type ModelRequest struct {
 
 // TeamResponse represents a response from the API containing team information.
 type TeamResponse struct {
-	TeamID         string                 `json:"team_id,omitempty"`
-	TeamAlias      string                 `json:"team_alias,omitempty"`
-	OrganizationID string                 `json:"organization_id,omitempty"`
-	Metadata       map[string]interface{} `json:"metadata,omitempty"`
-	TPMLimit       int                    `json:"tpm_limit,omitempty"`
-	RPMLimit       int                    `json:"rpm_limit,omitempty"`
-	MaxBudget      float64                `json:"max_budget,omitempty"`
-	BudgetDuration string                 `json:"budget_duration,omitempty"`
-	Models         []string               `json:"models"`
-	Blocked        bool                   `json:"blocked,omitempty"`
+	TeamID                string                 `json:"team_id,omitempty"`
+	TeamAlias             string                 `json:"team_alias,omitempty"`
+	OrganizationID        string                 `json:"organization_id,omitempty"`
+	Metadata              map[string]interface{} `json:"metadata,omitempty"`
+	TPMLimit              int                    `json:"tpm_limit,omitempty"`
+	RPMLimit              int                    `json:"rpm_limit,omitempty"`
+	MaxBudget             float64                `json:"max_budget,omitempty"`
+	BudgetDuration        string                 `json:"budget_duration,omitempty"`
+	Models                []string               `json:"models"`
+	Blocked               bool                   `json:"blocked,omitempty"`
+	TeamMemberPermissions []string               `json:"team_member_permissions,omitempty"`
 }
 
 // LiteLLMParams represents the parameters for LiteLLM.
